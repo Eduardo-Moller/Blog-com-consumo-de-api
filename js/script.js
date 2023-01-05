c97cd916258f5071abbccd1b9bdcf04e9a68632a
Eduardo Luís <93298848+Eduardo-Moller@users.noreply.github.com>
2023-01-04 23:35:55 -0300

    Atualização

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-function api(url, callback) {
-  let xml = new XMLHttpRequest();
-  xml.onreadystatechange = function () {
-    if (xml.readyState == 4 && xml.status == 200) {
-      let x = JSON.parse(xml.responseText);
-      callback(x);
-    }
-  };
-  xml.open("GET", url, true);
-  xml.send();
-}
-api("https://api.thecatapi.com/v1/images/search?size=full", function (x) {
-  var html = '<img src="' + x[0]["url"] + '">';
-  document.getElementById("conteudo").innerHTML = html;
+angular.module("blog", []);
+angular.module("blog").controller("Rest", function ($scope, $http) {
+  $http
+    .get("https://api.thecatapi.com/v1/images/search?size=full")
+    .success(function (data) {
+      var url = data[0]["url"];
+      $scope.url = url;
+    });
 });
